878561f2f1d2ea7bf85fee7ac4513f71c44b4942
Jorge Tinoco <jorge.tinoco@kfc.com.ec>
2019-06-25 20:13:41 -0500

    Se corrige bug de función toISOSTring

--- GenerarArchvioCaptura-DataFast/index.js
+++ GenerarArchvioCaptura-DataFast/index.js
@@ -1,12 +1,12 @@
 module.exports = async function(context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
+    if (req.query) {
         context.res = {
             // status: 200, /* Defaults to 200 */
             request: req.query,
             body: {
-                date: new Date().toISOString
+                date: new Date().toISOString()
             }
         };
     }
